bf06d2012cbe4ce27ed5d2998c232ba8f4835905
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2021-04-11 20:03:07 +0100

    Update backend.tf

--- Sec09/Lab9.3/backend.tf
+++ Sec09/Lab9.3/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend azurerm {
       resource_group_name  = "Terra-rg"
       storage_account_name = "remotesa10"
-      container_name       = "azcontainer"
-      key                  = "Lab9.tfstate"
+      container_name       = "kashcontainer"
+      key                  = "Labgit.tfstate"
   }
 }
